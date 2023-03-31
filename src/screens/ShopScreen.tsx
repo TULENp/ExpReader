c8f79f1a1ac1e65bddf767ccc1164a7eed6b60c8
kildanL <kildan325@gmail.com>
2023-03-31 16:08:55 +0300

    Add carousel in ShopScreen

--- src/screens/ShopScreen.tsx
+++ src/screens/ShopScreen.tsx
@@ -1,18 +1,40 @@
-import { View, Text, KeyboardAvoidingView, FlatList, ImageBackground, StatusBar, Image } from 'react-native'
+import { View, Text, KeyboardAvoidingView, FlatList, ImageBackground, StatusBar, Image, ImageSourcePropType, Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import { stylesShopScreen } from './stylesScreen'
 import { books } from '../TestData/books';
 import { BookShopCard } from '../components/BookShopCard';
 import { deepBlue } from '../constants/colors';
 import { Input } from '@rneui/themed';
+import Carousel from 'react-native-reanimated-carousel';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const width = Dimensions.get('window').width;
 
 export default function ShopScreen() {
     const [searchText, setSearchText] = useState<string>('');
+    // const ads = [{
+    //   ad1:require('../../assets/Ad1.png')
+    // },
+    // {
+    //   ad2:require('../../assets/Ad2.png')
+    // },
+    // {
+    //   ad3:require('../../assets/Ad3.png')
+    // }]
+    const ads = [{
+      img:require('../../assets/Ad1.png')
+    },
+    {
+      img:require('../../assets/Ad2.png')
+    },
+    {
+      img:require('../../assets/Ad3.png')
+    }
+    ]
     
     return (
         <>
           <KeyboardAvoidingView behavior='height' style={stylesShopScreen.shop_page}>
-            
             <FlatList ListHeaderComponent={(
                 <>
                   <StatusBar backgroundColor = {deepBlue}/>
@@ -27,6 +49,15 @@ export default function ShopScreen() {
                     <Image style={{width:36, height:36, }} source={require('../../assets/heartRed.png')}/>
                     <Image style={{width:36, height:36, }} source={require('../../assets/filter.png')}/>
                   </ImageBackground>
+                  <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                    <GestureHandlerRootView>
+                      <Carousel  width={width} autoPlay={true}  scrollAnimationDuration={1000} height={151}   data={ads} renderItem={({item}) => 
+                          <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+                            <Image style={{width:364, height:151}} source={item.img}/>
+                          </View>
+                      } />
+                    </GestureHandlerRootView>
+                  </View>
                 </>
             )} 
                     data={books} renderItem={(item) => <BookShopCard />}>
