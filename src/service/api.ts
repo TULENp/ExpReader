5372ea79a375d6e732c7a3165e7d343b7eeabfd5
kildanL <kildan325@gmail.com>
2023-04-13 11:52:19 +0300

    fix bug in PostUpdateBooks

--- src/service/api.ts
+++ src/service/api.ts
@@ -159,22 +159,23 @@ const UBStats = [{
 
 export async function PostUpdateBooks(){
     const token = await getTokenAS();
-    if(!token) {
-        return axios.post('user/updateUserBookStat',{
-            UBStat:[{
-                "bookID": 1,
-                "readPages": 69,
-                "currentPage": 23,
-                "isRead": false,
-                "readDate": ""
-            }]
+    if(!token) return '401';
+    
+    return axios.post('user/updateUserBookStat',{
+        UBStat:[{
+            "bookID": 1,
+            "readPages": 69,
+            "currentPage": 23,
+            "isRead": false,
+            "readDate": ""
+        }]
         },
         {
             headers:{
                 Authorization: token
             }
         }).catch(error=> error.response.status)
-    }
+    
 }
 
 
