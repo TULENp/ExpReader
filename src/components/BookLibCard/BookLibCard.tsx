3c07774b8f099a8f52532cf2304907883a260e32
Eugene <zh_kononenko3@mail.ru>
2023-04-20 18:37:02 +0300

    Fix cover. NOT FINISHED

--- src/components/BookLibCard/BookLibCard.tsx
+++ src/components/BookLibCard/BookLibCard.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, ImageBackground, Button, Pressable } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { LibStackParams, TLibBook, TRarity } from '../../types'
 import { stylesBookLibCard } from './style';
 import { Shadow } from 'react-native-shadow-2';
@@ -11,13 +11,17 @@ import { DownloadBook } from '../../service/api';
 import { coversDir, booksDir, fileBooksDir, imageURL } from '../../constants';
 import * as FileSystem from 'expo-file-system';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { FileInfo } from 'expo-file-system';
+import { AppContext } from '../../context/AppContext';
 
 export function BookLibCard({ book }: { book: TLibBook }) {
+    const { netInfo } = useContext(AppContext);
     const { navigate } = useNavigation<NavigationProp<LibStackParams>>();
     const { authors, bookPages, cover, currentPage, id, isRead, readDate, readPages, title, fileName } = book;
     const percent = Math.floor((readPages / bookPages) * 100) || 0;
     const [bookRarity, setBookRarity] = useState<TRarity>();
     const [isDownloaded, setIsDownloaded] = useState(false);
+    const [isCoverExists, setIsCoverExists] = useState(true);
 
 
     useEffect(() => {
@@ -29,12 +33,15 @@ export function BookLibCard({ book }: { book: TLibBook }) {
     }, [isDownloaded])
 
     async function checkDownload() {
-        const shopBooks: string[] = await FileSystem.readDirectoryAsync(booksDir);
-        const fileBooks: string[] = await FileSystem.readDirectoryAsync(fileBooksDir);
-        const bookNames = shopBooks.concat(fileBooks);
-        if (bookNames.includes(fileName)) {
+        const shopBookInfo: FileInfo = (await FileSystem.getInfoAsync(booksDir + fileName));
+        const fileBookInfo: FileInfo = (await FileSystem.getInfoAsync(fileBooksDir + fileName));
+        if (shopBookInfo.exists || fileBookInfo.exists) {
             setIsDownloaded(true);
         }
+        // const coverInfo: FileInfo = await FileSystem.getInfoAsync(coversDir + cover);
+        // if (!coverInfo.isDirectory && coverInfo.exists) {
+        //     setIsCoverExists(true);
+        // }
     }
 
     //TODO add download loading
@@ -48,6 +55,7 @@ export function BookLibCard({ book }: { book: TLibBook }) {
     //TODO remove deleteBook func on build ver
     async function deleteBook() {
         await FileSystem.deleteAsync(booksDir + fileName, { idempotent: true });
+        await FileSystem.deleteAsync(coversDir + cover, { idempotent: true });
         setIsDownloaded(false);
         console.log('deleted');
     }
@@ -65,13 +73,14 @@ export function BookLibCard({ book }: { book: TLibBook }) {
         <Pressable onPress={readOrDownloadBook}>
             <View style={stylesBookLibCard.container_lib_book}>
                 <Shadow distance={1} startColor={bookRarity?.color} offset={[7, 6]}>
-                    {cover === ""
+                    {isCoverExists
                         ?
+                        <ImageBackground style={stylesBookLibCard.cover_book} source={{ uri: coversDir + cover }}
+                            onError={() => setIsCoverExists(false)} />
+                        :
                         <View style={stylesBookLibCard.empty_cover_book}>
                             <Text style={stylesBookLibCard.text_empty_cover_book}>{title}</Text>
                         </View>
-                        :
-                        <ImageBackground style={stylesBookLibCard.cover_book} source={{ uri: coversDir + cover }} />
                     }
                 </Shadow>
                 <View style={stylesBookLibCard.container_info_book}>
