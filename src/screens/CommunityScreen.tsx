6b0d5ad2b509a58678bb836b58237723c438c499
kildanL <kildan325@gmail.com>
2023-05-10 00:07:05 +0300

    stylized profile screen

--- src/screens/CommunityScreen.tsx
+++ src/screens/CommunityScreen.tsx
@@ -1,13 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import { UserCard } from '../components/UserCard'
+import { stylesShopScreen } from './stylesScreen';
+import { Input } from '@rneui/themed';
 
 export function CommunityScreen() {
 
+    const [searchSubscription,setSearchSubscription] = useState<string>('');
+
     return (
-        <View>
-            <Text>    Поисковая строка</Text>
-            <Text>    Все / Рекомендованные / Подписки</Text>
+        <ScrollView style={{flex:1,backgroundColor:'white',paddingLeft:13,paddingRight:13}}>
+            <View style={[stylesShopScreen.container_search_input,{width:'100%'}]}>
+				<Input
+					onChangeText={(text) => {
+                        setSearchSubscription(text)
+                        }}
+					placeholder={'Найти пользователей'}
+					inputContainerStyle={{ borderBottomWidth: 0 }}
+					leftIcon={{ type: 'octicons', name: 'search' }}
+					style={[stylesShopScreen.search_input, { fontFamily: 'MontserratAlt400',}]} />
+			</View>
+            {/* <Text>    Все / Рекомендованные / Подписки</Text> */}
             <UserCard userData={{
                 nickname: 'Антон',
                 readPagesNum: 0,
@@ -39,6 +52,6 @@ export function CommunityScreen() {
                 isSub: false,
             }} />
 
-        </View>
+        </ScrollView>
     )
 }
