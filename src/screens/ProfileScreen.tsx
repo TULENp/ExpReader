829599ee7baf9d44d37740182c0b265b7b80dbbc
Eugene <zh_kononenko3@mail.ru>
2023-05-15 19:11:50 +0300

    Add profile favorites check

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -146,20 +146,22 @@ export function ProfileScreen() {
 								</Pressable> */}
 
 								{/* Favorites */}
-								<View style={{ marginTop: 20, paddingLeft: 13, paddingRight: 13 }}>
-									<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-										<Text style={[stylesProfileScreen.text_points, { color: 'black', fontSize: 16 }]}>Любимый жанр: </Text>
-										<View style={stylesBookScreen.container_genres}>
-											<Text style={stylesBookScreen.text_genres}>{userData.favGenre}</Text>
+								{(userData.favAuthor || userData.favGenre) &&
+									<View style={{ marginTop: 20, paddingLeft: 13, paddingRight: 13 }}>
+										<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+											<Text style={[stylesProfileScreen.text_points, { color: 'black', fontSize: 16 }]}>Любимый жанр: </Text>
+											<View style={stylesBookScreen.container_genres}>
+												<Text style={stylesBookScreen.text_genres}>{userData.favGenre}</Text>
+											</View>
+										</View>
+										<View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 15 }}>
+											<Text style={[stylesProfileScreen.text_points, { color: 'black', fontSize: 16 }]}>Любимый автор: </Text>
+											<View style={[stylesBookScreen.container_genres]}>
+												<Text style={[stylesBookScreen.text_genres, { color: deepBlue }]}>{userData.favAuthor}</Text>
+											</View>
 										</View>
 									</View>
-									<View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 15 }}>
-										<Text style={[stylesProfileScreen.text_points, { color: 'black', fontSize: 16 }]}>Любимый автор: </Text>
-										<View style={[stylesBookScreen.container_genres]}>
-											<Text style={[stylesBookScreen.text_genres, { color: deepBlue }]}>{userData.favAuthor}</Text>
-										</View>
-									</View>
-								</View>
+								}
 
 								{/* Achievements */}
 								<Pressable onPress={() => navigate('Achievements')} style={stylesProfileScreen.container_achievements}>
