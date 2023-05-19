9182151a7cb32c53b5f7d1471b0cd50f9bcad96d
kildanL <kildan325@gmail.com>
2023-05-19 22:39:01 +0300

    add build changes

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -10,7 +10,6 @@ const srcImgHarryPotter3 = require('../../assets/harryPotter3.jpg');
 const srcImgHarryPotter4 = require('../../assets/harryPotter4.png');
 const srcImg451 = require('../../assets/451.png');
 const srcImgNoir = require('../../assets/noir.jpg');
-const srcImgSilver = require('../../assets/silver.jpg');
 const srcIcnBook = require('../../assets/book.png');
 const srcIcnPoints = require('../../assets/coin.png');
 const srcIcnSetting = require('../../assets/settings.png');
@@ -39,7 +38,7 @@ const srcIcnBooks = require('../../assets/books.png');
 export {
     srcImgLibraryHeader, srcImgHarryPotter3,
     srcImgHarryPotter4, srcImg451, srcImgNoir,
-    srcImgSilver, srcIcnBook, srcImgShopHeader,
+    srcIcnBook, srcImgShopHeader,
     srcIcnRedHeart, srcIcnFilter, srcImgProfileHeader,
     srcIcnPoints, srcIcnSetting, srcIcnBronze, srcIcnSilver,
     srcIcnGold, srcIcnBack, srcIcnBackWhite, srcImgBookHeader,
